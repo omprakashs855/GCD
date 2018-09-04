e580fa207d040f54e182cbd9e90945b26372036e
omprakashs855 <37749355+omprakashs855@users.noreply.github.com>
2018-09-04 19:53:32 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int fun_modulo(int f[],int g[],int m,int n,int *pos);
+void check(int t);
 int main()
 {
-    int f[20],g[20],c[20];
-    int i,m,n,t=0;
+    int f[20],g[20],s[20];
+    int m,n,i,k;
+    int pos,t,t1=0,op;
 
     printf("Enter the number of Frame bit = ");
     scanf("%d",&m);
@@ -17,7 +19,8 @@ int main()
     for(i=0;i<n;i++)
         scanf("%d",&g[i]);
 
-    int k=n-1;
+    k=n-1;
+    pos = m;
     for(i=m;i<(m+k);i++)
         f[i]=0;
     printf("The Full Frame polynomial is:= ");
@@ -25,51 +28,76 @@ int main()
         printf("%d",f[i]);
     printf("\n");
     //Modulo 2 division
-    while(t==0){
-    int q=n,j;
-    int s=f[0];
-    for(i=0;i<n;i++)
-        c[i]=f[i];
-
-    for(i=0;i<m;i++)
-    {
-        if(s==1){
-            for(j=0;j<n;j++)
-              c[j]=c[j]^g[j];
-            }
-        if(s==0){
-            for(j=0;j<n;j++)
-                c[j]=c[j]^0;
-            }
-
-        for(j=0;j<n-1;j++)
-            c[j]=c[j+1];
-        if(i!=(m-1)){
-        c[n-1]=f[q];
-        }
-        else
-            break;
-        q++;
-        s=c[0];
-    }
-    printf("The CheckSum:\n");
-    int t2=0;
-    for(i=0;i<(n-1);i++){
-        printf("%d",c[i]);
-        if(c[i]==1)
-            t2++;
-    }
-    if(t2==0)
-        break;
-    int t1=0;
-    for(i=m;i<(m+k);i++)
-        f[i]=c[t1++];
+    t=fun_modulo(f,g,m,n,&pos);
+    check(t);
     printf("\nTransmitted CRC code = ");
     for(i=0;i<(m+k);i++)
         printf("%d",f[i]);
     printf("\n");
-    printf("\nWish to transmit the CRC code.....press 0 else press any key:");
-    scanf("%d",&t);
+    printf("Do you want to enter new data to transmit:");
+    printf("\nPress '1' for Yes and '0' for No : ");
+    scanf("%d",&op);
+    if(op==1)
+    while(t1==0)
+    {
+        pos=n;
+        printf("Enter any other data = ");
+        for(i=0;i<m+k;i++)
+            scanf("%d",&s[i]);
+        t=fun_modulo(f,g,m,n,&pos);
+        check(t);
+        printf("\nWish to Continue...Press 0 else any value:");
+        scanf("%d",&t1);
     }
     return 0;
+}
+int fun_modulo(int f[],int g[],int m,int n,int *pos)
+{
+    int k,q,j,s,t1,t2;
+    int i,c[20];
+    k=n-1;
+            q=n;
+            s=f[0];
+            for(i=0;i<n;i++)
+                c[i]=f[i];
+            for(i=0;i<m;i++)
+            {
+                   for(j=0;j<n;j++)
+                   {
+                       if(s==1)
+                        c[j]=c[j]^g[j];
+                       if(s==0)
+                        c[j]=c[j]^0;
+                   }
+                   for(j=0;j<n-1;j++)
+                    c[j]=c[j+1];
+                   if(i!=(m-1))
+                    {
+                       c[n-1]=f[q];
+                    }
+                   else
+                     break;
+                   q++;
+                   s=c[0];
+            }
+            printf("The CheckSum:\n");
+            t2=0;
+            for(i=0;i<(n-1);i++){
+               printf("%d",c[i]);
+            if(c[i]==1)
+               t2++;
+    }
+    t1=0;
+    for(i=m;i<(m+k);i++){
+        f[*pos]=c[t1++];
+        *pos = *pos+1;
+    }
+    return t2;
+}
+void check(int t)
+{
+    if(t>0)
+        printf("\nError");
+    else
+        printf("\nNo Error");
 }
